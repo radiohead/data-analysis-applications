e9fb42bda100c58b631435ea0f4f6849219aff29
Igor Suleymanoff <igorsuleymanoff@gmail.com>
2016-01-19 22:01:46 +0200

    exercise 1, heart diseases data set

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'knn', github: 'radiohead/knn'
---
+++ main.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'knn'
+require 'csv'
+
+def run(data, type)
+  means_and_vars = (2..15).map do |number_of_neighbors|
+    target_index = data.first.size - 1
+
+    leave_one_out = Validations::LeaveOneOut.new(data, target_index, number_of_neighbors)
+    number_of_neighbors, mean_error, error_variance = leave_one_out.send(type)
+
+    puts "#{type.capitalize}: Mean error for K = #{number_of_neighbors} is #{mean_error}"
+    puts "#{type.capitalize}: Variance for   K = #{number_of_neighbors} is #{error_variance}"
+
+    [number_of_neighbors, mean_error, error_variance]
+  end
+
+  puts "#{type.capitalize}: Least mean error is #{means_and_vars.min_by { |vals| vals[1] }}"
+  puts "#{type.capitalize}: Least variance is #{means_and_vars.min_by { |vals| vals[2] }}"
+  puts "#{type.capitalize}: Best overall is #{means_and_vars.min_by { |vals| (vals[1] + vals[2]) / 2 }}"  
+end
+
+csv = CSV.open('data/processed.cleveland.data')
+data = csv.map{ |row| row.map{ |c| c.to_i rescue 0 } }
+run(data, :regression)
+run(data, :classification)
